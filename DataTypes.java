247f7f86ec76a402886b394839a12c93fac69565
nk8021115-sudo <nk8021115@gmail.com>
2026-03-23 10:17:08 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+/*
+DECLARE x : INTEGER
+DECLARE y : INTEGER
+DECLARE z : INTEGER
+READ x
+READ y
+z : x+y
+PRINT z
+ */
+
+package Collage_Class.PseudoCode;
+import java.util.*;
+public class DataTypes {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+        int z;
+
+        z = x + y;
+        System.out.println(z);
+    }
+}
